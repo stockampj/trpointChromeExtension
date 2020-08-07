9133ece66d95941adeb4c70c54236153d2376394
Joel Stockamp <stockampj@gmail.com>
2020-08-06 17:24:10 -0700

    change the structure so that the url of the site changes which scripts are loaded and executed. It will also add a function to download multiple files at once from the email recipient list

---
+++ js/flashDriveDownloader.js
@@ -0,0 +1,51 @@
+function downloadAllFiles(){
+  
+  let links = document.querySelectorAll("a");
+  let filteredLinks = [];
+  links.forEach((link)=>{
+    let id = link.id;
+    if (id.slice(0,8)===`GridView`) {
+      let targetId = $(`#${id}`)[0];
+      filteredLinks.push(targetId);
+    }
+  })
+  console.log(filteredLinks.length);
+  function fileDownloaderScript(){
+    
+  
+    let interval= setInterval(clickFunction,1500,filteredLinks);
+    let count = 0;
+    function clickFunction(filteredLinks) {
+      let node = filteredLinks.pop();
+      count +=1;
+      node.click();
+      if (filteredLinks.length == 0) {
+        console.log(count)
+        clearInterval(interval);
+      }
+    }
+  }
+  
+  function addButtonPanel() {
+    let jqTarget = $('#flashdrive-tools')[0];
+    if (jqTarget === undefined){
+      function buttonScript(){
+        fileDownloaderScript();
+      }
+      let tableTarget = document.querySelectorAll("table")[4];
+      let newRow = document.createElement('DIV');
+      newRow.id ='flashdrive-tools';
+      tableTarget.append(newRow)
+      jqTarget = $('#flashdrive-tools')[0]; 
+      jqTarget.innerHTML = `<button id="flashdrive-button" class="btn btn-primary">Download All (${filteredLinks.length})</button>`;
+      $("#flashdrive-button")[0].addEventListener("click", function(event){
+        event.preventDefault()
+        buttonScript();
+      });
+      // $('#flashdrive-button')[0].css("margin", "5px 0px 5px 0px");
+    };
+  }
+  addButtonPanel();
+}
+
+downloadAllFiles();
--- js/scriptChooser.js
+++ js/scriptChooser.js
@@ -1,12 +1,14 @@
 function siteLocation(url) {
   let scriptName;
   if (url.includes("https://trpoint.com/App1/TCApp/Docs/TxnDocuments")){
-    scriptName="flashDriveFilter"
-  } else {
+    scriptName="flashDriveFilter";
+  } else if (url.includes("https://www.trpoint.com/DocAccess/DocAccess")) {
+    scriptName="flashDriveDownloader";
+  }else {
     return false;
   }
   return scriptName;
 }
 
 let helperScript = siteLocation(window.location.href)
-// console.log(helperScript);
+console.log(helperScript);
